e792548b1b0b5ee8ecbed8d83f4b9bb55c144afc
Jacqueline Lucena <jacqueline.lucena@aluno.senai.br>
2023-11-28 21:31:11 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@ let alunos = [
     { nome: "Maurao", email: "maurao@hotmail.com", telefone: 21944488576, profissao: "frontend" },
     { nome: "Ramonzao", email: "monzao@gmail.com", telefone: 2173484973, profissao: "frontend" },
     { nome: "Davizao", email: "davi@msn.com", telefone: 21975584073, profissao: "frontend" },
-  
 ];
+
 // carregar os dados do array na tabela
 window.onload = (event) => {
-    let tbody = document.querySelector("#mytable");
+    let tbody = document.querySelector("#myTable");
 alunos.forEach((aluno, index) => {
-    
+    // 1 momento
     let tr = document.createElement("tr");
     tr.id = index +1;
     let tdCod = document.createElement("td");
@@ -28,77 +28,69 @@ alunos.forEach((aluno, index) => {
     let tdTelefone = document.createElement("td");
     let tdProfissao = document.createElement("td");
     let tdBotao = document.createElement("td");
-
+// 2 momento 
     tdCod.textContent = index;
     tdNome.textContent = aluno.nome;
     tdEmail.textContent = aluno.email;
     tdTelefone.textContent = aluno.telefone;
     tdProfissao.textContent = aluno.profissao;
     tdBotao.innerHTML = `<button class="btn-remover" onclick="remover(this)" >Remover</button>`
-
+// 3momento adicionar os elementos na row
     tr.appendChild(tdCod);
     tr.appendChild(tdNome);
     tr.appendChild(tdEmail);
     tr.appendChild(tdTelefone);
     tr.appendChild(tdProfissao);
     tr.appendChild(tdBotao);
-
+    // 4 momento adicionar o tr no tbody
     tbody.appendChild(tr);
-
-
 });
-
 
 }
 
 function remover (id){
-let row = id.parentNode.parentNode.id;
-row = document.getElementById(row);
-row.parentNode.removeChild(row);
-return false
+    let row = id.parentNode.parentNode.id;
+    row = document.getElementById(row);
+    row.parentNode.removeChild(row);
+    return false
 }
 
-function adicionar (){
-//Definindo as vareaveis e recebendo os dados
-let nome = document.getElementById('nome').value;
-let email = document.getElementById('email').Value;
-let telefone = document.getElementById('telefone').value;
-let profissao = document.getElementById('profissao').value;
-let table = document.getElementById('mytable');
+function adicionar(){
+    //Definindo as variaveis e recebendo os dados
+    let nome = document.getElementById('nome').value;
+    let email =document.getElementById('email').value;
+    let telefone =document.getElementById('telefone').value;
+    let profissao = document.getElementById('profissao').value;
+    let table = document.getElementById('myTable');
 
-//calculando o tamanho da tabela
-let tableSize = table.rows.length;
+    //Calculando o tamanho da Tabela
+    let tableSize = table.rows.length;
+    //Inserindo uma linha abaixo da Tabela
+    let row = table.insertRow(tableSize);
+    let col1 = row.insertCell(0);
+    let col2 = row.insertCell(1);
+    let col3 = row.insertCell(2);
+    let col4 = row.insertCell(3);
+    let col5 = row.insertCell(4);
+    let col6 = row.insertCell(5);
+     //Adicionando o id no elemento a ser criado
+    row.id = tableSize;
+    //Criando o codigo do botão para remover a linha
+    let btnCode = "<button class='remove-btn' onclick='remover(this)'>Remover</button>";
+  //Preenchendo as celulas da linha
+  console.log(tableSize)
+    col1.innerHTML = tableSize;
+    col2.innerHTML = nome;
+    col3.innerHTML = email;
+    col4.innerHTML = telefone;
+    col5.innerHTML = profissao;
+    col6.innerHTML = btnCode;
+ //Limpando os campos de inserção de dados
+    document.getElementById('nome').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('telefone').value = "";
+    document.getElementById('profissao').value = "";
+//Retornando 'false' para impedir o reload da pagina
+    return false
 
-//inserindo uma linha abaixo da tabela
-let row = table.insertRow(tableSize);
-let col1 = row.insertCell(0);
-let col2 = row.insertCell(1);
-let col3 = row.insertCell(2);
-let col4 = row.insertCell(3);
-let col5 = row.insertCell(4);
-let col6 = row.insertCell(5);
-
-//adicinando o id no elemento a ser criado
-row.id = tableSize;
-
-  //Criando o codigo do botão para remover a linha
-let btnCode = "<button class='remove-btn' onlick='remover(this)'>Remover</button>"
-
-//preenchendo as celulas da linha
-col1.innerHTML = tableSize;
-col2.innerHTML = nome;
-col3.innerHTML = email;
-col4.innerHTML = telefone;
-col5.innerHTML = profissao;
-col6.innerHTML = btnCode;
-
-//limpando os campos de inserção dos dados 
-document.getElementById('nome').value = "";
-document.getElementById('email').Value = "";
-document.getElementById('telefone').value = "";
-document.getElementById('profissao').value = "";
-
-//retornando 'false' para impedrir o reload da pagina
-
-return false
 }
