15497375b5c85ae7064c8c0c18ad2015ffd3171a
Jacqueline Lucena <jacqueline.lucena@aluno.senai.br>
2023-11-30 21:22:52 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // definindo um array de alunos
 let alunos = [
-    { 
-        nome: "Aninhha", 
-        email: "ninha@gmail.com", 
-        telefone: 21935484773, 
-        profissao: "frontend" 
+    {
+        nome: "Aninhha",
+        email: "ninha@gmail.com",
+        telefone: 21935484773,
+        profissao: "frontend"
     },
     { nome: "Jaque", email: "jaque@gmail.com", telefone: 21974450073, profissao: "backend" },
     { nome: "Julinha", email: "juju@msn.com", telefone: 21979984073, profissao: "fullstack" },
@@ -13,60 +13,68 @@ let alunos = [
     { nome: "Maurao", email: "maurao@hotmail.com", telefone: 21944488576, profissao: "frontend" },
     { nome: "Ramonzao", email: "monzao@gmail.com", telefone: 2173484973, profissao: "frontend" },
     { nome: "Davizao", email: "davi@msn.com", telefone: 21975584073, profissao: "frontend" },
+
 ];
 
 // carregar os dados do array na tabela
 window.onload = (event) => {
-    let tbody = document.querySelector("#tableBody");
-alunos.forEach((aluno, index) => {
-    // 1 momento
-    let tr = document.createElement("tr");
-    tr.id = index +1;
-    let tdCod = document.createElement("td");
-    let tdNome = document.createElement("td");
-    let tdEmail = document.createElement("td");
-    let tdTelefone = document.createElement("td");
-    let tdProfissao = document.createElement("td");
-    let tdBotao = document.createElement("td");
-// 2 momento 
-    tdCod.textContent = index;
-    tdNome.textContent = aluno.nome;
-    tdEmail.textContent = aluno.email;
-    tdTelefone.textContent = aluno.telefone;
-    tdProfissao.textContent = aluno.profissao;
-    tdBotao.innerHTML = `<button class="btn btn-danger" onclick="remover(this)" >Remover</button>`
-// 3momento adicionar os elementos na row
-    tr.appendChild(tdCod);
-    tr.appendChild(tdNome);
-    tr.appendChild(tdEmail);
-    tr.appendChild(tdTelefone);
-    tr.appendChild(tdProfissao);
-    tr.appendChild(tdBotao);
-    // 4 momento adicionar o tr no tbody
-    tbody.appendChild(tr);
-    initPieChart();
-});
+    let tbody = document.querySelector("tBody");
+    alunos.forEach((aluno, index) => {
+        let tr = document.createElement("tr");
+        tr.id = index + 1;
+
+        let tdCod = document.createElement("td");
+        let tdNome = document.createElement("td");
+        let tdEmail = document.createElement("td");
+        let tdTelefone = document.createElement("td");
+        let tdProfissao = document.createElement("td");
+        let tdBotao = document.createElement("td");
+
+        tdCod.textContent = index;
+        tdNome.textContent = aluno.nome;
+        tdEmail.textContent = aluno.email;
+        tdTelefone.textContent = aluno.telefone;
+        tdProfissao.textContent = aluno.profissao;
+        tdBotao.innerHTML = `<button class="btn btn-danger" onclick="remover(this)" >Remover</button>`
+
+        tr.appendChild(tdCod);
+        tr.appendChild(tdNome);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdTelefone);
+        tr.appendChild(tdProfissao);
+        tr.appendChild(tdBotao);
+
+        tbody.appendChild(tr);
+
+        initPieChart()
+
+
+    });
+
 
 }
 
-function remover (id){
+function remover(id) {
     let row = id.parentNode.parentNode.id;
     row = document.getElementById(row);
     row.parentNode.removeChild(row);
     return false
 }
 
-function adicionar(){
-    //Definindo as variaveis e recebendo os dados
-    let nome = document.getElementById('nome').value;
-    let email =document.getElementById('email').value;
-    let telefone =document.getElementById('telefone').value;
-    let profissao = document.getElementById('profissao').value;
-    let table = document.getElementById('tableBody');
+// Definindo as variaveis e recebendo os dados
 
-    //Calculando o tamanho da Tabela
+function adicionar() {
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let profissao = document.getElementById('profissao').value;
+    let table = document.getElementById('myTable');
+
+
+    // calculando o tamanho da tabela
     let tableSize = table.rows.length;
-    //Inserindo uma linha abaixo da Tabela
+
+    // inserindo uma linha abaixo da tabela
     let row = table.insertRow(tableSize);
     let col1 = row.insertCell(0);
     let col2 = row.insertCell(1);
@@ -74,37 +82,40 @@ function adicionar(){
     let col4 = row.insertCell(3);
     let col5 = row.insertCell(4);
     let col6 = row.insertCell(5);
-     //Adicionando o id no elemento a ser criado
+
+    // Adicionando o id no elemento a ser criado
     row.id = tableSize;
-    //Criando o codigo do botão para remover a linha
+
+    // Criando o codigo do botão para remover a linha
     let btnCode = "<button class='btn btn-danger' onclick='remover(this)'>Remover</button>";
-  //Preenchendo as celulas da linha
-  console.log(tableSize)
+
+    // Preenchendo as celulas da linha
     col1.innerHTML = tableSize;
     col2.innerHTML = nome;
     col3.innerHTML = email;
     col4.innerHTML = telefone;
     col5.innerHTML = profissao;
     col6.innerHTML = btnCode;
- //Limpando os campos de inserção de dados
+
+    // Limpando os campos de inseção de dados
     document.getElementById('nome').value = "";
     document.getElementById('email').value = "";
     document.getElementById('telefone').value = "";
     document.getElementById('profissao').value = "";
-//Retornando 'false' para impedir o reload da pagina
+    // Retornando "false" para impedir o reload da
     return false
 
-};
+}
 
 const initPieChart = () => {
     const ctx = document.getElementById("myChart");
     const data = {
-        labels: ['Frontend', 'Backend', 'Fullstack','Mobile'],
+        labels: ['Frontend', 'Backend', 'Fullstack', 'Mobile'],
         datasets: [
             {
                 label: 'Quantidade',
                 data: [10, 20, 50, 20],
-                backgroundColor:  ['pink', 'lightblue', 'lightgreen', 'orange'],
+                backgroundColor: ['pink', 'lightblue', 'lightgreen', 'orange'],
                 hoverOffset: 4
             }
         ]
@@ -116,5 +127,38 @@ const initPieChart = () => {
             type: 'pie',
             data: data
         }
+    )
+};
+
+const initPolarAreaChart = () => {
+    removerChart();
+    // 1 passo: pegar o elemento #myChart
+    const ctx = document.getElementById('myChart');
+
+    //2 passo: definir os dados do grafico
+    const data = {
+        labels: ['Frontend', 'Backend', 'Fullstack','Mobile'],
+        datasets: [{
+            label: 'Quantidade',
+            data: [ 11, 15, 7, 3],
+            backgroundColor: ['pink', 'lightblue', 'lightgreen', 'orange']
+        }]
+    };
+
+    const polarAreaChart = new Chart(
+        ctx,
+        {
+            type: 'polarArea',
+            data: data
+        }
     );
+};
+
+
+const removerChart = () => {
+    const divChart = document.querySelector('.chart');
+    document.getElementById("myChart").remove();
+    const myChart = document.createElement("canvas");
+    myChart.id="myChart";
+    divChart.appendChild(myChart);
 };
